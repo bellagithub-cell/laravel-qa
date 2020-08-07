cd2fced7f92ad97f9f1fb383249c9dc03603aab4
bellagithub-cell <bella.utomo@student.umn.ac.id>
2020-08-07 23:45:12 +0700

    Display all questions

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// karena kita gk cuma get, tp post, delete dll jadi pakai
+Route::resource('questions', 'QuestionsController');
+// ini akan di handle oleh questionscontroller
